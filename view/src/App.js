d43dfc6a86e79e691ee3c21559ea9f7758761eea
GBlachly <gblachly4@gmail.com>
2023-01-23 17:07:19 -0500

    Added Router and Link Routes in Layout

--- view/src/App.js
+++ view/src/App.js
@@ -1,24 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Layout } from './pages/Layout';
+import { Home } from './pages/Home';
+import { Products } from './pages/Products';
+import { Login } from './pages/Login';
+import { Register } from './pages/Register';
+import { Account } from './pages/Account';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p className='text-success'>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path='/' element={<Layout />}>
+          <Route index element={<Home />}/>
+          <Route path='products' element={<Products />}/>
+          <Route path='login' element={<Login />}/>
+          <Route path='register' element={<Register />}/>
+          <Route path='account' element={<Account />}/>
+        </Route>
+      </Routes>
+    </Router>
   );
 }
 
